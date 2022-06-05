7a62f382140477ae49b2f27c1c90b235236d7fef
Nikita <nikita@platform500.com>
2022-06-05 21:12:41 +0300

    new commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzBlock from './components/PizzaBlock';
+
+import pizzas from './assets/pizzas.json';
+
+import './scss/app.scss';
+console.log(pizzas);
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => (
+              <PizzBlock
+                title={obj.title}
+                price={obj.price}
+                image={obj.imageUrl}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => setActiveIndex(index)}
+            className={activeIndex === index ? 'active' : ''}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
