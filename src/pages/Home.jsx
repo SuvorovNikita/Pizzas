20b03b72bea13ee522a60bbfad4f9f9a1bfbe78b
Nikita <nikit.suvorov@gmail.com>
2022-06-08 18:56:40 +0300

    pagination and search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-function Home() {
+function Home({ searchValue }) {
   const [items, setItems] = useState([]);
   const [isLodiang, setIsLoading] = useState(true);
   const [categoryID, setCategoryID] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sortProperty: 'rating',
@@ -20,9 +22,10 @@ function Home() {
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sortType.sortProperty.replace('-', '');
     const category = categoryID > 0 ? `category=${categoryID}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     fetch(
-      `https://629dfa3f3dda090f3c10b803.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order},
+      `https://629dfa3f3dda090f3c10b803.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search},
       )}`,
     )
       .then((res) => res.json())
@@ -31,7 +34,10 @@ function Home() {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryID, sortType]);
+  }, [categoryID, sortType, searchValue, currentPage]);
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const pizzas = items.map((obj) => <PizzBlock key={obj.id} {...obj} />);
 
   return (
     <div className="container">
@@ -40,11 +46,8 @@ function Home() {
         <Sort value={sortType} onChangeSort={(index) => setSortType(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLodiang
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLodiang ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 }
