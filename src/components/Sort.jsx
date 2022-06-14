00a71ba8de7871e733d3bd5cabb548d027c1bb0c
Nikita <nikit.suvorov@gmail.com>
2022-06-14 20:21:53 +0300

    fixed popup sort

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { setSort } from '../redux/slices/filterSlice';
@@ -13,6 +13,7 @@ function Sort() {
   // sort price
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filters.sort);
+  const sortRef = useRef();
 
   const [open, setOpen] = useState(false);
 
@@ -21,8 +22,20 @@ function Sort() {
     setOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.path.includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleClickOutside);
+
+    return () => document.body.removeEventListener('click', handleClickOutside);
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
